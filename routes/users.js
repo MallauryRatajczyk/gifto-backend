4d1d5e14595f2692535221995518e364b571e7fa
Your name <marine.marchandb@etik.com>
2024-10-28 12:34:14 +0100

    creation model et schema users + mongodb

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,53 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+require('../models/connection');
+const User = require('../models/users');
+const { checkBody } = require('../modules/checkBody');
+const uid2 = require('uid2');
+const bcrypt = require('bcrypt');
+
+// GET user.
+router.get('/', function(req, res) {
+  res.json({});
+}); //
+
+// GET user id.
+router.get('/:id', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+// POST user enregistrer.
+router.post('/enregistrer', (req, res) => {
+  if (!checkBody(req.body, ['nom', 'email', 'password'])) {
+    res.json({ result: false, error: 'Champ non saisi.' });
+    return;
+  }
+
+  // Check if the user has not already been registered
+  User.findOne({ email: req.body.emaail }).then(data => {
+    if (data === null) {
+      const hash = bcrypt.hashSync(req.body.password, 10);
+
+      const newUser = new User({
+        nom: req.body.nom,
+        email: req.body.email,
+        password: hash,
+        token: uid2(32),
+      });
+
+      newUser.save().then(newDoc => {
+        res.json({ result: true, token: newDoc.token });
+      });
+  }});
+});
+
+// POST user login.
+
+// PUT user id.
+
+// PUT user id/historique.
+
+// DELETE user id.
 
 module.exports = router;
