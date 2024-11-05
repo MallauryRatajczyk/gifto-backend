14e84f88c164049c5cd7088a8e2a53d37b61e463
Mallaury <mallaury.ratajczyk@gmail.com>
2024-11-05 09:29:52 +0100

    sprint 4

--- models/demande.js
+++ models/demande.js
@@ -11,8 +11,8 @@ const demandeSchema = mongoose.Schema({
   possesseur: { type: String, required: true },
   demandeur: { type: String, required: true },
   type: typeSchema,
-  message: [{ de: String, a: String, message: String, date: { type: Date, default: Date.now() } }],
-  statut: { type: String, enum: ['pending', 'accepted', 'declined', "read"], default: 'pending' },
+  message: [{ de: String, a: String, message: { type: String, required: true }, date: { type: Date, default: Date.now() } }],
+  statut: { type: String, enum: ['pending', 'accepted', 'declined', 'read'], default: 'pending' },
   dateCreation: { type: Date, default: Date.now() },
   dateMAJ: Date,
 });
